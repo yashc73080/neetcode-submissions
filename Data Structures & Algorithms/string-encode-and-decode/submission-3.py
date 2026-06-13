c1d8310c4fe518212405ecf437fcfa3bb7f73967
Yash Chennawar <144838733+yashc73080@users.noreply.github.com>
2026-06-13 18:33:39 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+
+        solution = ""
+        for word in strs:
+            solution += str(len(word))
+            solution += "#"
+            solution += word
+
+        return solution
+
+    def decode(self, s: str) -> List[str]:
+        
+        solution = []
+        count = ""
+
+        i = 0
+        while i < len(s):
+            if s[i] != "#":
+                count += s[i]
+                i += 1
+
+            elif s[i] == "#":
+                count = int(count)
+                i += 1
+                splice = s[i:i+count]
+                print(splice)
+                solution.append(splice)
+                i += count
+                count = ""
+
+        return solution
